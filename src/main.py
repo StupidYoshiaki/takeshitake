579ab4e9fdb6bec5eb7999c1ca6dbb8447b8445b
StupidYoshiaki <nyanyap0418@gmail.com>
2024-12-19 15:16:19 +0900

    a

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
 import re
+
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord.ui import Select, View, Button
+
 import asyncio
 import unicodedata
 from pykakasi import kakasi
@@ -87,8 +90,8 @@ def hconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
     im_list_resize = [cv2.resize(im, (int(im.shape[1] * h_min / im.shape[0]), h_min), interpolation=interpolation) for im in im_list]
     return cv2.hconcat(im_list_resize)
 
-# 画像生成関数
-def create_slot_image(base_images_path, output_path, number_images_path, num_selection=3):
+# パチンコ画像生成関数
+def create_pachinko_image(base_images_path, output_path, number_images_path, num_selection=3):
     # ベース画像をランダムに10枚選択して数字を割り当てる
     base_images = [os.path.join(base_images_path, img) for img in os.listdir(base_images_path)]
     base_images = random.sample(base_images, 8)
@@ -112,11 +115,52 @@ def create_slot_image(base_images_path, output_path, number_images_path, num_sel
 
     # 画像を横に連結
     processed_images_cv = [cv2.cvtColor(np.array(img), cv2.COLOR_RGBA2BGRA) for img in processed_images]
-    slot_image = hconcat_resize_min(processed_images_cv)
-    # slot_image = cv2.resize(slot_image, dsize=None, fx=1.5, fy=1.5)
+    pachinko_image = hconcat_resize_min(processed_images_cv)
+    # pachinko_image = cv2.resize(pachinko_image, dsize=None, fx=1.5, fy=1.5)
     
     # 結果を保存
-    cv2.imwrite(output_path, slot_image)
+    cv2.imwrite(output_path, pachinko_image)
+
+# テロップ画像生成関数
+def create_telop_image(base_images_path, output_path, text):
+    # 元の画像を読み込む
+    img = Image.open(base_images_path)
+    draw = ImageDraw.Draw(img)
+
+    font_size = 80 - (len(text)//15 - 1) * 10
+    # フォントの設定
+    font = ImageFont.truetype("./font/hiragino.ttc", font_size)
+
+    # テキストと配置を設定
+    width, height = img.size
+    x, y, x2, y2 = draw.textbbox((width / 2, height-15), text, font=font, anchor='md')
+    x, y, x2, y2 = int(x), int(y), int(x2), int(y2)
+
+    # 小さなキャンバスを作成して文字列を描画
+    text_width, text_height = x2 - x + 10, y2 - y + 10
+    small_canvas_size = (text_width, text_height)
+    small_canvas = Image.new("RGBA", small_canvas_size)  # 透明なキャンバス
+
+    # 小さなキャンバスにテキストを描画
+    small_draw = ImageDraw.Draw(small_canvas)
+    small_draw.text(
+        (0, 0),  # 左上から描画
+        text,
+        font=font,
+        fill="#C0C0C0",
+        stroke_width=6,
+        stroke_fill="#171717"
+    )
+
+    # 解像度を下げる（リサイズ処理）
+    low_res_canvas = small_canvas.resize((text_width // 2, text_height // 2), Image.NEAREST)
+    upscaled_canvas = low_res_canvas.resize((text_width, text_height), Image.NEAREST)
+
+    # 元の画像に文字列を貼り付け
+    img.paste(upscaled_canvas, (x, y), upscaled_canvas)
+
+    # 結果を保存
+    img.save(output_path)
 
 
 # ログイン時の処理
@@ -147,26 +191,14 @@ async def on_message(message):
             result = yomi_to_filename[content]
             file_path = f"./img/raw/{result}.png"
             await message.channel.send(file=discord.File(file_path))
-            
-    # # メンションされていない場合、メッセージから勝手に反応する
-    # else:
-    #     content = normalize_text(content)
-    #     # 2文字以下の投稿には反応しない
-    #     if len(content) <= 2:
-    #         return
-    #     content = get_filename(content)
-    #     if content:
-    #         result = yomi_to_filename[content]
-    #         file_path = f"./img/raw/{result}.png"
-    #         await message.channel.send(file=discord.File(file_path))
 
 @tree.command(name="pachinko", description="CR竹下家", guild=discord.Object(id=int(os.getenv("GUILD_ID"))))
 async def pachinko(interaction: discord.Interaction):
     await interaction.response.defer()
     base_images_path = "./img/raw"
-    output_path = "./output/slot.png"
-    number_images_path = "./img/slot"
-    create_slot_image(base_images_path, output_path, number_images_path)
+    output_path = "./output/pachinko.png"
+    number_images_path = "./img/pachinko"
+    create_pachinko_image(base_images_path, output_path, number_images_path)
     await interaction.followup.send(file=discord.File(output_path))
     
 @tree.command(name="goroku",description="語録検索",guild=discord.Object(id=int(os.getenv("GUILD_ID"))))
@@ -180,6 +212,47 @@ async def goroku(interaction: discord.Interaction,text:str):
     else:
         text = "ねえよニワカが＾＾；"
         await interaction.response.send_message(text)
+
+class TelopView(View):
+    def __init__(self, text, interaction):
+        super().__init__()
+        self.text = text
+        self.interaction = interaction
+        self.selected_person = None
+
+        # セレクトメニューを追加
+        self.select_menu = Select(
+            placeholder="選択してください",
+            options=[
+                discord.SelectOption(label="ジジイ", value="ジジイ"),
+                discord.SelectOption(label="ババア", value="ババア"),
+                discord.SelectOption(label="桜庭さん", value="桜庭さん"),
+            ]
+        )
+        self.select_menu.callback = self.select_callback
+        self.add_item(self.select_menu)
+
+    async def select_callback(self, interaction: discord.Interaction):
+        self.selected_person = self.select_menu.values[0]
+        await interaction.response.defer()  # 選択を確定
+
+        # 画像生成処理
+        base_images_path = f"./img/telop/{self.selected_person}.png"
+        output_path = "./output/telop.png"
+        create_telop_image(base_images_path, output_path, self.text)
+
+        # 画像を返信
+        await self.interaction.followup.send(file=discord.File(output_path))
+        
+        self.selected_menu.disabled = True  # メニューを無効化
+        self.stop()  # Viewを終了
+
+
+@tree.command(name="telop", description="テロップ", guild=discord.Object(id=int(os.getenv("GUILD_ID"))))
+async def telop(interaction: discord.Interaction, text: str):
+    view = TelopView(text, interaction)
+    await interaction.response.send_message("誰を選びますか？", view=view)
+    
 
 server_thread()
 client.run(TOKEN)
